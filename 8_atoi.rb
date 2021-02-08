d6b55970a5ba8130b05dc69150523471498fc11f
naosuke <info@naosuke.me>
2021-02-09 03:35:46 +0900

    :umbrella: Wrong Ansewer - Input: "+1" - Output: 0 - Expected: 1

---
+++ 8_atoi.rb
@@ -0,0 +1,32 @@
+# @param {String} s
+# @return {Integer}
+def my_atoi(s)
+  num_str = ''
+  num_flag = false
+  cond = 1 # default: positive
+
+  unless /^\-?[\d]+/.match? s.strip[0..1]
+    return 0
+  end
+
+  s.strip.each_char do |c|
+    break if !/[-\d]/.match?(c) && num_flag
+
+    if c == '-'
+      cond = -1
+      next
+    end
+
+    num_str << c
+    num_flag = true
+  end
+  num = num_str.to_i * cond
+
+  if num > 2**31 - 1
+    return 2**31 - 1
+  elsif num < -1 * 2**31
+    return -1 * 2**31
+  else
+    return num
+  end
+end
