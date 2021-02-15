50f6abdb75428104b7e195f18f7bd0f156143387
naosuke <naosuke@ntt.dev>
2021-02-16 03:08:51 +0900

    :sunny: Accepted

--- 010/13_roman_to_integer.rb
+++ 010/13_roman_to_integer.rb
@@ -2,9 +2,14 @@
 # @return {Integer}
 def roman_to_int(s)
   num = 0
-  mapping.keys.each do |k|
-    /^#{k}+/.match(s) { |m| num += mapping[k] * m[0].length / k.length }
-    s.gsub!(/^#{k}+/, '')
+
+  s.each_char.with_index do |c, i|
+    current_value = mapping[c]
+    unless s[i+1].nil?
+      current_value >= mapping[s[i+1]] ? num += current_value : num -= current_value
+    else
+      num += current_value
+    end
   end
 
   num
@@ -12,18 +17,12 @@ end
 
 def mapping
   @chars ||= {
-    'M'  => 1000,
-    'CM' => 900,
-    'D'  => 500,
-    'CD' => 400,
-    'C'  => 100,
-    'XC' => 90,
-    'L'  => 50,
-    'XL' => 40,
-    'X'  => 10,
-    'IX' => 9,
-    'V'  => 5,
-    'IV' => 4,
-    'I'  => 1
+    'M' => 1000,
+    'D' => 500,
+    'C' => 100,
+    'L' => 50,
+    'X' => 10,
+    'V' => 5,
+    'I' => 1
   }
 end
