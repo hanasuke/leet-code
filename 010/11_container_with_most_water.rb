a0af8cd870daabe6cdd7b19310b00c557983faa2
naosuke <naosuke@ntt.dev>
2021-02-16 01:42:20 +0900

    :umbrella: Time Limit Exceeded

--- 010/11_container_with_most_water.rb
+++ 010/11_container_with_most_water.rb
@@ -2,11 +2,18 @@
 # @return {Integer}
 def max_area(height)
   max_area = 0
+  max_area_height = 0
+  max_area_width = 0
   height.each_with_index do |h, w1|
     (w1+1).upto(height.length-1) do |w2|
+      next if height[w1] < max_area_height && max_area_width > (w2-w1)
       area = (height[w1] > height[w2]) ? height[w2] * (w2 - w1) : height[w1] * (w2-w1)
 
-      max_area = area if max_area < area
+      if max_area < area
+        max_area = area
+        max_area_height = (height[w1] > height[w2]) ? height[w2] : height[w1]
+        max_area_width = w2 - w1
+      end
     end
   end
 
